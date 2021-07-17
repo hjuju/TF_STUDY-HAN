3a5b21ddf0a40f51ae0e015636571fe22e21d31a
hjuju <tkackeowjs@naver.com>
2021-07-17 21:17:49 +0900

    new

--- keras/keras28_mnist2_cnn.py
+++ keras/keras28_mnist2_cnn.py
@@ -22,19 +22,20 @@ import time
 x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255 # 3차원 -> 4차원  // 데이터의 내용과 순서가 바뀌면 안됨
 x_test = x_test.reshape(10000, 28, 28, 1).astype('float32')/255
 
-ic(x_train.shape)
-ic(x_test.shape)
+# ic(x_train.shape) # (60000, 28, 28, 1)
+# ic(x_test.shape) # (10000, 28, 28, 1)
 
 
 ic(np.unique(y_train)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 one = OneHotEncoder()
 y_train = y_train.reshape(-1,1)
 y_test = y_test.reshape(-1,1)
+ic(y_train.shape)            # (60000,1)
+ic(y_test.shape) # (10000, 1)
 one.fit(y_train)
 y_train = one.transform(y_train).toarray()
 y_test = one.transform(y_test).toarray()
-# ic(y_train.shape)            # (60000,10)
-# ic(y_test.shape)
+
 
 
 
@@ -45,34 +46,36 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
 model = Sequential()
 model.add(Conv2D(filters=100, kernel_size=(2,2), padding='same', input_shape=(28, 28, 1)))
-model.add(Conv2D(20, (2,2), activation='relu'))               
-model.add(Conv2D(30, (2,2), activation='relu', padding='valid'))                
-model.add(MaxPooling2D())                                     
-model.add(Conv2D(20, (2,2), activation='relu'))                              
+model.add(Conv2D(40, (2,2), activation='relu'))               
+model.add(Conv2D(30, (2,2), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(20, (2,2), activation='relu'))                                                                               
 model.add(Flatten())                                          
+model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
-model.add(Dense(128, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
-model.add(Dense(10, activation='sigmoid')) # 이진분류로 출력
+model.add(Dense(10, activation='softmax')) # 이진분류로 출력
 
 #3. 컴파일, 훈련 metrics=['acc']
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 start = time.time()
-es = EarlyStopping(monitor='acc', patience=5, mode='auto', verbose=1)
-model.fit(x_train, y_train, epochs=1000, verbose=1, batch_size=32, callbacks=[es])
-걸린시간 = (time.time() - start) /60
+es = EarlyStopping(monitor='loss', patience=5, mode='auto', verbose=1)
+model.fit(x_train, y_train, epochs=1000, verbose=1, validation_split= 0.001, batch_size=16, callbacks=[es])
+걸린시간 = round((time.time() - start) /60,1)
 
 #4. 평가, 예측 predict 필요x acc로 판단
 
 loss = model.evaluate(x_test, y_test)
 ic('loss:', loss[0])
 ic('accuracy', loss[1])
-ic(걸린시간)
+ic(f'{걸린시간}분')
 # ic(loss)
 
 '''
-ic| 'loss:', loss[0]: 0.01058739423751831
-ic| 'accuracy', loss[1]: 0.9908000230789185
+
+ic| 'loss:', loss[0]: 0.02277880162000656
+ic| 'accuracy', loss[1]: 0.9912999868392944
+
 '''
