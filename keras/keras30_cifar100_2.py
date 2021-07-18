471a2e129f7ec1a4138869c8d11894313f94d41d
hjuju <tkackeowjs@naver.com>
2021-07-18 18:06:13 +0900

    new

--- keras/keras30_cifar100_2.py
+++ keras/keras30_cifar100_2.py
@@ -6,6 +6,8 @@ from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Input, Conv2D, Flatten, MaxPooling2D
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.utils import to_categorical
+from icecream import ic
+import time
 
 #1. data preprocessing
 (x_train, y_train), (x_test, y_test) = cifar100.load_data()
@@ -18,7 +20,7 @@ y_test = to_categorical(y_test)
 
 
 
-#2. modeling
+#2. 모델링
 input = Input(shape=(32, 32, 3))
 x = Conv2D(64, (3,3), padding='same', activation='relu')(input)
 x = MaxPooling2D((2,2))(x)
@@ -33,15 +35,18 @@ output = Dense(100, activation='sigmoid')(x)
 
 model = Model(inputs=input, outputs=output)
 
-#3. compiling, training
-es = EarlyStopping(monitor='accuracy', patience=5, mode='auto', verbose=1)
+#3. 컴파일, 훈련
+es = EarlyStopping(monitor='acc', patience=5, mode='auto', verbose=1)
 model.compile(loss='binary_crossentropy', optimizer='adam', 
-                        metrics=['accuracy'])
+                        metrics=['acc'])
+start = time.time()
 model.fit(x_train, y_train, epochs=1000, batch_size=128, 
                                 validation_split=0.001, callbacks=[es])
+걸린시간 = round((time.time() - start) /60,1)
 
 #4. evaluating, prediction
 loss = model.evaluate(x_test, y_test)
 
 print('loss = ', loss[0])
 print('accuracy = ', loss[1])
+ic(f'{걸린시간}분')
