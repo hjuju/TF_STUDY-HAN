7592bb2b8fcfa9f81c234604e7781e6222d71742
hjuju <tkackeowjs@naver.com>
2021-07-20 12:51:39 +0900

    new

---
+++ keras/keras34_RNN.py
@@ -0,0 +1,35 @@
+import numpy as np
+from icecream import ic
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN
+
+#1. 데이터
+x = np.array([[1,2,3],[2,3,4], [3,4,5], [4,5,6]])
+y = np.array([4,5,6,7])
+
+ic(x.shape, y.shape) # x.shape: (4, 3), y.shape: (4,)
+
+x = x.reshape(4, 3, 1)
+# (batch_size, timesteps, feature) // 각 timesteps의 feature단위로 연산
+# RNN은 3차원 데이터 입력해줘야 하기 때문에 3차원으로 변경
+
+#2. 모델 구성
+model = Sequential()
+model.add(SimpleRNN(units=8, activation='relu', input_shape=(3,1))) # reshape 한 차원으로 입력, RNN 모델 구성 시 맨 처음에 simpleRNN으로 input shape만 맞춰주면 됨
+# 파라미터 수 ( unit 개수 * unit 개수 ) + ( input_dim(feature) 수 * unit 개수 ) + ( 1 * unit 개수) => (10*10) + (1*10) + (1*10) = 120
+model.add(Dense(8, activation='relu'))
+model.add(Dense(6, activation='relu'))
+model.add(Dense(2, activation='relu'))
+model.add(Dense(1))
+
+model.summary()
+
+#3. 컴파일, 훈련
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x,y,epochs=128, batch_size=1)
+
+#4. 평가, 예측
+x_input = np.array([5,6,7]).reshape(1,3,1) # 3차원으로 변경 후 넣어줌
+result = model.predict(x_input)
+ic(result) # [[8.066318]]
