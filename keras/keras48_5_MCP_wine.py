694d42ecb7121064c70ea125d753cfa4689aa042
hjuju <tkackeowjs@naver.com>
2021-07-21 19:06:37 +0900

    new

---
+++ keras/keras48_5_MCP_wine.py
@@ -0,0 +1,95 @@
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler, QuantileTransformer, PowerTransformer, StandardScaler, OneHotEncoder
+from tensorflow.keras.callbacks import EarlyStopping
+import numpy as np
+from icecream import ic
+from tensorflow.keras.models import Sequential
+from sklearn.model_selection import train_test_split
+import time
+from sklearn.metrics import r2_score
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import Dense, Conv1D, Flatten, Dropout, MaxPool2D, LSTM 
+from tensorflow.keras.models import Sequential, load_model
+
+
+
+#1. 데이터
+datasets = pd.read_csv('../_data/winequality-white.csv', sep=';', index_col=None, header=0 )
+
+# ic(datasets)
+
+x = datasets.iloc[:,0:11]
+y = datasets.iloc[:,[-1]]
+
+# ic(x, y)
+# ic(x.shape, y.shape) 
+
+ic(np.unique(y))
+
+y = OneHotEncoder().fit_transform(y).toarray()
+
+
+# ic(y)
+
+
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.7, random_state=60) # train 309, test 133
+
+# scaler = QuantileTransformer()
+# scaler = StandardScaler()
+# scaler = PowerTransformer()
+scaler = RobustScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+
+ic(x_train.shape, x_test.shape)
+
+
+model = Sequential()
+model.add(LSTM(40, activation='relu', input_shape=(11,1), return_sequences=True))
+model.add(Conv1D(128, 2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(16, activation='relu'))
+model.add(Flatten())
+model.add(Dense(7, activation='softmax'))
+
+model.summary()
+
+#3. 컴파일, 훈련
+es = EarlyStopping(monitor='acc', patience=20, mode='auto', verbose=1)
+cp = ModelCheckpoint(monitor='val_loss', patience=10, mode='auto', save_best_only=True, filepath='./_save/ModelCheckPoint/keras48_5_MCP_wine.hdf5')
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+start = time.time()
+model.fit(x_train, y_train, epochs=1000, verbose=1, validation_split=0.2, batch_size=32, shuffle=True, callbacks=[es, cp])
+걸린시간 = round((time.time() - start) /60,1)
+
+model.save('./_save/ModelCheckPoint/keras48_5_model_save_wine.h5')
+
+#4. 평가, 예측
+
+y_predict = model.predict(x_test)
+loss = model.evaluate(x_test, y_test)
+ic(loss[0])
+ic(loss[1])
+ic(f'{걸린시간}분')
+
+'''
+
+모델, 체크포인트 저장
+
+
+모델 로드
+
+
+체크포인트 로드
+
+
+
+'''
---
+++ keras/keras48_7_MCP_fashion_mnist.py
@@ -0,0 +1,88 @@
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler, QuantileTransformer, PowerTransformer, StandardScaler, MinMaxScaler, OneHotEncoder
+import numpy as np
+from tensorflow.keras.datasets import fashion_mnist
+from icecream import ic
+import time
+import pandas as pd
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import Dense, Conv1D, Flatten, Dropout, MaxPool2D, LSTM 
+from tensorflow.keras.models import Sequential, load_model
+
+
+
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data() # x_train.shape: (60000, 28, 28), y_train.shape: (10000, 28, 28)
+
+ic(x_train.shape, y_train.shape)
+
+x_train = x_train.reshape(60000, 28 * 28)
+x_test = x_test.reshape(10000, 28 * 28)
+
+
+# 전처리 
+
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train) 
+x_test = scaler.transform(x_test)
+
+x_train = x_train.reshape(60000, 28 * 28 , 1)
+x_test = x_test.reshape(10000, 28 * 28 , 1)
+
+
+ic(np.unique(y_train))
+
+one = OneHotEncoder()
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+
+one.fit(y_train)
+y_train = one.transform(y_train).toarray()
+y_test = one.transform(y_test).toarray()
+
+
+
+
+
+model = Sequential()
+model.add(LSTM(10, activation='relu', input_shape=(28 * 28, 1 )))
+model.add(Conv1D(128, 2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(16, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+#3. 컴파일, 훈련
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+es = EarlyStopping(monitor='acc', patience=10, mode='auto', verbose=1)
+start = time.time()
+model.fit(x_train, y_train, epochs=10, verbose=1, validation_split=0.2, batch_size=1024, shuffle=True, callbacks=[es])
+걸린시간 = round((time.time() - start) /60,1)
+
+#4. 평가, 예측
+
+y_predict = model.predict(x_test)
+loss = model.evaluate(x_test, y_test)
+ic(loss[0])
+ic(loss[1])
+ic(f'{걸린시간}분')
+
+'''
+
+모델, 체크포인트 저장
+
+
+모델 로드
+
+
+체크포인트 로드
+
+
+
+'''
