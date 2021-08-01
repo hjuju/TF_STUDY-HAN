8f4de511c1557c86facc37d4af4f06d477da8667
hjuju <tkackeowjs@naver.com>
2021-08-02 01:04:42 +0900

    new

--- Dacon/01newstopic_2.py
+++ Dacon/01newstopic_2.py
@@ -39,7 +39,7 @@ test_text = test["cleaned_title"].tolist()
 
 train_label = np.asarray(train.topic_idx)
 
-tfidf = TfidfVectorizer(analyzer='word', sublinear_tf=True, ngram_range=(1, 2), max_features=150000, binary=False)
+tfidf = TfidfVectorizer(analyzer='word', sublinear_tf=True, ngram_range=(1, 2), max_features=45000, binary=False)
 
 tfidf.fit(train_text)
 
@@ -56,39 +56,15 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Embedding, Bidirectional, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 
-input1 = Input(shape=(150000,))
-emb = Embedding(input_dim=150000, output_dim=200, input_length=14)(input1)
-bid1 = LSTM(512, return_sequences=True, activation='relu')(input1)
-bid2 = Bidirectional(LSTM(128, return_sequences=True, activation='relu'))(bid1)
-drp = Dropout(0.3)(bid2)
-bid3 = Bidirectional(LSTM(256, return_sequences=True, activation='relu'))(drp)
-gru1 = GRU(512, activation='relu', return_sequences=True)(bid3)
-drp2 =Dropout(0.3)(gru1)
-gru2 = GRU(256, activation='relu', return_sequences=True)(drp2)
-flt1 = Flatten()(gru1)
-ds1 = Dense(128,activation='relu')(flt1)
-
-output11 = Dense(256, activation='relu')(ds1)
-drp = Dense(0.4)(output11)
-output12 = Dense(128, activation='relu')(drp)
-output13 = Dense(64, activation='relu')(output12)
-
-output21 = Dense(256, activation='relu')(ds1)
-output22 = Dense(128, activation='relu')(output21)
-drp = Dropout(0.3)(output22)
-output23 = Dense(64, activation='relu')(drp)
-
-merge1 = concatenate([output13, output23])
-merge2 = Dense(32, activation='relu')
-last_output = Dense(7, activation='softmax')(merge2)
-
-model = Model(inputs= input1, outputs=last_output)
-
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model = Sequential()
+model.add(Dense(512, input_dim=45000, activation='relu'))
+model.add(Dropout(0.6))
+model.add(Dense(7, activation='softmax'))
 
 import time
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['acc'])
 start_time = time.time()
-model.fit(train_tf_text[:40000], train_label[:40000], epochs=7, batch_size=16, validation_data=(train_tf_text[40000:], train_label[40000:]))
+model.fit(train_tf_text[:40000], train_label[:40000], epochs=10, batch_size=16, validation_data=(train_tf_text[40000:], train_label[40000:]))
 # model.fit(train_tf_text, train_label, epochs=5, batch_size=128, validation_split=0.2)
 duration_time = time.time() - start_time
 
