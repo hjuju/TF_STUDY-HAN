e38e0542ffcc8f83225b0ba895d25918e33f56b2
hjuju <tkackeowjs@naver.com>
2021-08-11 16:03:08 +0900

    pipeline

---
+++ Machine_Learning/ml09_pipeline4_RF_wine.py
@@ -0,0 +1,57 @@
+import numpy as np
+from sklearn.datasets import load_wine
+from icecream import ic
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import accuracy_score
+
+### 머신러닝(evaluate -> score)
+
+datasets = load_wine()
+
+# 1. 데이터
+x = datasets.data
+y = datasets.target
+ic(x.shape, y.shape)  # (150, 4), (150,)->(150, 3)
+
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
+
+# 1-2. 데이터 전처리
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+
+
+# 2. 모델(머신러닝에서는 정의만 해주면 됨)
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression # 로지스틱회귀는 분류모델(회귀모델 X)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline, Pipeline
+
+model = make_pipeline(MinMaxScaler(), RandomForestClassifier()) # 스케일러, 모델
+
+# model = LogisticRegression()
+# 3. 훈련(컴파일 포함되어 있어서 컴파일 할 필요 없음)
+model.fit(x_train, y_train)
+
+
+# 4. 평가(evaluate 대신 score 사용함!!), 예측
+
+# print('최적의 매개변수: ', model.best_estimator_) # cv를 통해 나온 값 / GridSearchCV를 통해서만 출력 가능
+# print("best_score: ", model.best_score_)
+
+model_score = model.score(x_test, y_test)
+y_predict = model.predict(x_test)
+acc = accuracy_score(y_test, y_predict)
+ic(model_score)
+ic(acc)
+
+'''
+RF
+ic| model_score: 1.0
+ic| acc: 1.0
+'''
+
---
+++ Machine_Learning/ml09_pipeline6_RF_diabets.py
@@ -0,0 +1,63 @@
+import numpy as np
+from sklearn.datasets import load_diabetes
+from icecream import ic
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import accuracy_score, r2_score
+
+### 머신러닝(evaluate -> score)
+
+datasets = load_diabetes()
+
+# 1. 데이터
+x = datasets.data
+y = datasets.target
+ic(x.shape, y.shape)  # (150, 4), (150,)->(150, 3)
+
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
+
+# 1-2. 데이터 전처리
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, PowerTransformer, QuantileTransformer
+
+
+# 2. 모델(머신러닝에서는 정의만 해주면 됨)
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression # 로지스틱회귀는 분류모델(회귀모델 X)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.pipeline import make_pipeline, Pipeline
+
+model = make_pipeline(QuantileTransformer(), RandomForestRegressor()) # 스케일러, 모델
+
+# model = LogisticRegression()
+# 3. 훈련(컴파일 포함되어 있어서 컴파일 할 필요 없음)
+model.fit(x_train, y_train)
+
+
+# 4. 평가(evaluate 대신 score 사용함!!), 예측
+
+# print('최적의 매개변수: ', model.best_estimator_) # cv를 통해 나온 값 / GridSearchCV를 통해서만 출력 가능
+# print("best_score: ", model.best_score_)
+
+model_score = model.score(x_test, y_test)
+y_predict = model.predict(x_test)
+R2 = r2_score(y_test, y_predict)
+ic(model_score)
+ic(R2)
+
+'''
+RF + MinMax
+ic| model_score: 0.3697998594001938
+ic| R2: 0.3697998594001938
+
+RF + standard
+ic| model_score: 0.37969785945504964
+ic| R2: 0.37969785945504964
+
+
+'''
+
