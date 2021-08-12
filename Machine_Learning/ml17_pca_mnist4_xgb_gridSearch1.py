f3a4223ffe3eedaeb34915a71dc40fafc9f2de70
hjuju <tkackeowjs@naver.com>
2021-08-12 22:40:32 +0900

    pca_mnist_xgb_grid

--- Machine_Learning/ml17_pca_mnist4_xgb_gridSearch1.py
+++ Machine_Learning/ml17_pca_mnist4_xgb_gridSearch1.py
@@ -15,11 +15,7 @@ import warnings
 from xgboost.sklearn import XGBClassifier
 warnings.filterwarnings('ignore') # 경고무시
 from sklearn.metrics import accuracy_score
-
-
-parameter = [{"n_estimators":[100, 200, 300], "learning_rate":[0.1,0.3,0.001,0.01], "max_depth":[4,5,6]},
-              {"n_estimators":[90, 100, 110], "learning_rate":[0.1,0.001,0.01], "max_depth":[4,5,6], "colsample_bytree":[0.6,0.9,1]},
-              {"n_estimators":[90, 110], "learning_rate":[0.1,0.001,0.5], "max_depth":[4,5,6], "colsample_bytree":[0.6,0.9,1],"colsample_bylevel":[0.6,0.7,0.9]}]
+from sklearn.decomposition import PCA
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -31,28 +27,47 @@ ic(x.shape, y.shape)  # (150, 4), (150,)->(150, 3)
 
 # ic| x.shape: (70000, 28, 28), y.shape: (70000,)
 
+compoents = 154
+
+x = x.reshape(x.shape[0], 28*28)
+# x.shape[1]
+pca = PCA(n_components=compoents)
+x = pca.fit_transform(x)
+# pca_EVR = pca.explained_variance_ratio_
+# cumsum = np.cumsum(pca_EVR)
+
+# ic(np.argmax(cumsum >= 0.95)+1) # ic| np.argmax(cumsum >= 0.999)+1: 486
 
 
-from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV, RandomizedSearchCV
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
 
+# ic(x_train.shape, x_test.shape, y_train.shape, y_test.shape, np.unique(y_train))
 
 
-# # 2. 모델(머신러닝에서는 정의만 해주면 됨)
 
-# n_splits=5
+parameter = [{"n_estimators":[100, 200, 300], "learning_rate":[0.1,0.3,0.001,0.01], "max_depth":[4,5,6]},
+              {"n_estimators":[90, 100, 110], "learning_rate":[0.1,0.001,0.01], "max_depth":[4,5,6], "colsample_bytree":[0.6,0.9,1]},
+              {"n_estimators":[90, 110], "learning_rate":[0.1,0.001,0.5], "max_depth":[4,5,6], "colsample_bytree":[0.6,0.9,1],"colsample_bylevel":[0.6,0.7,0.9]}]
 
-# kfold = KFold(n_splits=n_splits, shuffle=True, random_state=66)
+
+# 2. 모델(머신러닝에서는 정의만 해주면 됨)
+
+n_splits=5
+
+kfold = KFold(n_splits=n_splits, shuffle=True, random_state=66)
 
  
-# model = GridSearchCV(XGBClassifier(), parameter, cv=kfold) # 사용할 모델, parameter(정의), cv 명시 /  텐서플로로 하면 모델에 텐서플로 모델이 들어가면 됨
-# # model = SVC(C=1, kernel='linear')
+model = RandomizedSearchCV(XGBClassifier(eval_metric='mlogloss'), parameter, cv=kfold, verbose=1) 
+# 사용할 모델, parameter(정의), cv 명시 /  텐서플로로 하면 모델에 텐서플로 모델이 들어가면 됨
+# model = SVC(C=1, kernel='linear')
 
-# model.fit(x_train,y_train)
+model.fit(x_train,y_train)
 
-# print('최적의 매개변수: ', model.best_estimator_) # cv를 통해 나온 값 / GridSearchCV를 통해서만 출력 가능
-# print("best_score: ", model.best_score_)
+print('최적의 매개변수: ', model.best_estimator_) # cv를 통해 나온 값 / GridSearchCV를 통해서만 출력 가능
+print("best_score: ", model.best_score_)
 
 
 
