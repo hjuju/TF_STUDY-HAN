89b9b96912891fe1d7a47259189688a6a10f88bd
hjuju <tkackeowjs@naver.com>
2021-08-12 17:43:29 +0900

    pca_mnist_CNN

--- Machine_Learning/ml17_pca_mnist3_cnn.py
+++ Machine_Learning/ml17_pca_mnist3_cnn.py
@@ -8,9 +8,11 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder, R
 from tensorflow.keras.callbacks import EarlyStopping
 import time
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling3D
 from sklearn.model_selection import train_test_split
 import time
+
+from tensorflow.python.keras.layers.pooling import MaxPooling2D
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data() # _ : 변수로 받지 않는다
 
@@ -19,7 +21,7 @@ ic(x_train.shape, x_test.shape) # x_train.shape: (60000, 28, 28), x_test.shape: 
 
 x = np.append(x_train, x_test, axis=0)
 ic(x.shape)# ic| x.shape: (70000, 28, 28)
-y = np.append(y_train, y_test)
+y = np.append(y_train, y_test, axis=0)
 
 
 compoents = 28*28
@@ -28,39 +30,43 @@ x = x.reshape(x.shape[0], 28*28)
 # x.shape[1]
 pca = PCA(n_components=compoents)
 x = pca.fit_transform(x)
-x = x.reshape(x.shape[0], compoents)
-
-
-
-# ic(x.shape)
-# ic(np.unique(y))
-
-x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle=True, train_size=0.8, random_state=66)
 
 # pca_EVR = pca.explained_variance_ratio_
 
 # cumsum = np.cumsum(pca_EVR)
 # ic(cumsum)
 
-# ic(np.argmax(cumsum >= 0.95)+1) # ic| np.argmax(cumsum >= 0.95)+1: 154
+# ic(np.argmax(cumsum >= 0.999)+1) # ic| np.argmax(cumsum >= 0.999)+1: 486
+
+
+x = x.reshape(x.shape[0], 28,28,1)
+
+
+ic(x.shape) # ic| x.shape: (70000, 486)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle=True, train_size=0.8, random_state=66)
+
+ic(x_train.shape, x_test.shape, y_train.shape, y_test.shape, np.unique(y_train))
+
+'''
+ic| x_train.shape: (56000, 486, 1, 1)
+    x_test.shape: (14000, 486, 1, 1)
+
+'''
+
 
 # 모델 구성 
 
 # DNN으로 구성하고, 기존 DNN과 비교(PCA한것과 안한 것)
 
-# one = OneHotEncoder()
-# y_train =y_train.reshape(-1,1)
-# y_test = y_test.reshape(-1,1)
-# ic(y_train.shape)            # (60000,1)
-# ic(y_test.shape) # (10000, 1)
-# one.fit(y_train)
-# y_train = one.transform(y_train).toarray()
-# y_test = one.transform(y_test).toarray()
-
-
 model = Sequential()
-model.add(Dense(units=256, activation='relu', input_shape=(compoents,)))
-model.add(Flatten())
+model.add(Conv2D(64, kernel_size=(2,2), padding='same', activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(32, (2,2), padding='same',activation='relu')) 
+# model.add(MaxPooling2D(2,padding='same'))            
+model.add(Conv2D(32, (2,2),padding='same', activation='relu'))
+model.add(Conv2D(16, (2,2),padding='same', activation='relu'))                   
+model.add(Conv2D(16, (2,2),padding='same', activation='relu'))                                                        
+model.add(Flatten())                                          
 model.add(Dense(128, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(10, activation='softmax')) 
@@ -87,15 +93,14 @@ ic(f'{걸린시간}분')
 # ic(loss)
 
 '''
-PCA(154)
-ic| 'loss:', loss[0]: 0.2959951162338257
-ic| 'accuracy', loss[1]: 0.9559285640716553
-ic| f'{걸린시간}분': '0.2분'
+# PCA(486)
+# ic| 'loss:', loss[0]: 0.2959951162338257
+# ic| 'accuracy', loss[1]: 0.9559285640716553
+# ic| f'{걸린시간}분': '0.2분'
 
-
-ic| 'loss:', loss[0]: 0.3436247706413269
-ic| 'accuracy', loss[1]: 0.9532856941223145
-ic| f'{걸린시간}분': '0.2분'
-
+PCA(X)
+ic| 'loss:', loss[0]: 0.35307228565216064
+ic| 'accuracy', loss[1]: 0.9562143087387085
+ic| f'{걸린시간}분': '4.5분'
 
 '''
