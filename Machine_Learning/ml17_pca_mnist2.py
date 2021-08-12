141a155d90575b77607585bf1061563b4dbd24a9
hjuju <tkackeowjs@naver.com>
2021-08-12 16:31:26 +0900

    pca_mnist_DNN

--- Machine_Learning/ml17_pca_mnist2.py
+++ Machine_Learning/ml17_pca_mnist2.py
@@ -3,17 +3,25 @@ from numpy.core.fromnumeric import reshape
 from tensorflow.keras.datasets import mnist
 from icecream import ic
 from sklearn.decomposition import PCA
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder, RobustScaler, QuantileTransformer, PowerTransformer
+from tensorflow.keras.callbacks import EarlyStopping
+import time
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from sklearn.model_selection import train_test_split
+import time
 
-(x_train, _), (x_test, _) = mnist.load_data() # _ : 변수로 받지 않는다
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # _ : 변수로 받지 않는다
 
 ic(x_train.shape, x_test.shape) # x_train.shape: (60000, 28, 28), x_test.shape: (10000, 28, 28)
-ic(_.shape)
+
 
 x = np.append(x_train, x_test, axis=0)
 ic(x.shape)# ic| x.shape: (70000, 28, 28)
 
 x = x.reshape(x.shape[0], 28*28)
-
+# x.shape[1]
 pca = PCA(n_components=154)
 x = pca.fit_transform(x)
 
@@ -28,19 +36,8 @@ x = pca.fit_transform(x)
 
 # DNN으로 구성하고, 기존 DNN과 비교(PCA한것과 안한 것)
 
-model = Sequential()
-model.add(Dense(units=100, activation='relu', input_shape=(28,28)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(10, activation='softmax')) 
-
-# 전처리
-
-
-ic(np.unique(y_train)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 one = OneHotEncoder()
-y_train = y_train.reshape(-1,1)
+y_train =y_train.reshape(-1,1)
 y_test = y_test.reshape(-1,1)
 ic(y_train.shape)            # (60000,1)
 ic(y_test.shape) # (10000, 1)
@@ -49,11 +46,24 @@ y_train = one.transform(y_train).toarray()
 y_test = one.transform(y_test).toarray()
 
 
+model = Sequential()
+model.add(Dense(units=154, activation='relu', input_shape=(28,28)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax')) 
+
+# 전처리
+
+
+# ic(np.unique(y_train)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
 #3. 컴파일, 훈련 metrics=['acc']
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 start = time.time()
 es = EarlyStopping(monitor='loss', patience=5, mode='auto', verbose=1)
-model.fit(x_train, y_train, epochs=1000, verbose=1, validation_split= 0.001, batch_size=128, callbacks=[es])
+model.fit(x_train, y_train, epochs=1000, verbose=1, validation_split= 0.1, batch_size=128, callbacks=[es])
 걸린시간 = round((time.time() - start) /60,1)
 
 #4. 평가, 예측 predict 필요x acc로 판단
@@ -63,3 +73,17 @@ ic('loss:', loss[0])
 ic('accuracy', loss[1])
 ic(f'{걸린시간}분')
 # ic(loss)
+
+'''
+PCA(154)
+ic| 'loss:', loss[0]: 0.20301192998886108
+ic| 'accuracy', loss[1]: 0.9725000262260437
+
+
+PCA(X)
+ic| 'loss:', loss[0]: 0.24869416654109955
+ic| 'accuracy', loss[1]: 0.9682999849319458
+ic| f'{걸린시간}분': '1.1분'
+
+
+'''
