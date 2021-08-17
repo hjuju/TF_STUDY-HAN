5d9cc91c1daba1203e8ac2cbc4327a7e52901c45
hjuju <tkackeowjs@naver.com>
2021-08-17 16:12:17 +0900

    outliers

--- Machine_Learning/ml28_outliers1.py
+++ Machine_Learning/ml28_outliers1.py
@@ -1,6 +1,12 @@
+# 이상치 처리
+#1. 삭제
+#2. NAN처리후 -> 보간 / linear
+#3. 결측치 처리 방법과 유사
+
+
 import numpy as np
 from numpy.lib.function_base import quantile
-aaa = np.array([1,2,-10000,4,5,6,7,8,90,100,5000])
+aaa = np.array([1,2,-1000,4,5,6,7,8,90,100,500])
 
 def outliers(data_out):
     quantile_1, q2, quantile_3 = np.percentile(data_out, [25,50,75])
@@ -15,3 +21,11 @@ def outliers(data_out):
 outliers_loc = outliers(aaa)
 
 print('이상치의 위치: ', outliers_loc)
+
+# 시각화
+# 위 데이터로 boxplot 그리기
+
+import matplotlib.pyplot as plt
+
+plt.boxplot(aaa)
+plt.show()
