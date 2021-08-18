a1ef79df6411434e6c2521718feb8082c2594e5e
hjuju <tkackeowjs@naver.com>
2021-08-18 11:52:25 +0900

    y_label matching

--- Machine_Learning/ml29_wine_quality4.py
+++ Machine_Learning/ml29_wine_quality4.py
@@ -9,8 +9,8 @@ from xgboost import XGBClassifier
 #1. 데이터
 datasets = pd.read_csv('../_data/wine/winequality-white.csv', index_col=None, header=0, sep=';')
 
-count_data = datasets.groupby('quality')['quality'].count()
-ic(count_data)
+# count_data = datasets.groupby('quality')['quality'].count()
+# ic(count_data)
 
 # ic(datasets.head())
 
@@ -23,6 +23,18 @@ datasets = datasets.values
 x = datasets[:,:11]
 y = datasets[:,11]
 
+newlist = [] # 라벨값을 0,1,2에 매칭 -> Y라벨 수장할 수 있는 조건이 있어야함
+for i in list(y):
+    if i <= 4:
+        newlist += [0]
+    elif i <= 7:
+        newlist += [1]
+    else:
+        newlist += [2]
+
+y = np.array(newlist)
+ic(y)
+
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 
@@ -30,22 +42,18 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.8, shuffle
 
 import matplotlib.pyplot as plt
 # count_data.plot()
-plt.bar(count_data.index, count_data)
-plt.show()
 
-
-
-# scaler = StandardScaler()
-# x_train = scaler.fit_transform(x_train)
-# x_test = scaler.transform(x_test)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
 
 #2. 모델
-# model = XGBClassifier(n_jobs=-1)
+model = XGBClassifier(n_jobs=-1)
 
-# model.fit(x_train, y_train)
+model.fit(x_train, y_train)
 
-# score = model.score(x_test, y_test)
+score = model.score(x_test, y_test)
 
-# print('acc: ', score) 
+print('acc: ', score) 
 
 # # acc:  0.6816326530612244
