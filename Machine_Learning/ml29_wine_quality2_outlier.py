e128ddc2208835507e5ecd699b25d8ab6ad11893
hjuju <tkackeowjs@naver.com>
2021-08-18 17:42:20 +0900

    outlier

--- Machine_Learning/ml29_wine_quality2_outlier.py
+++ Machine_Learning/ml29_wine_quality2_outlier.py
@@ -6,6 +6,9 @@ from sklearn.datasets import load_wine
 from icecream import ic
 from xgboost import XGBClassifier
 
+aaa = np.array([[1,2,10000,3,4,6,7,8,90,100,5000],
+                [1000,2000,3,4000,5000,6000,7000,8,9000,10000,1001]])
+                
 #1. 데이터
 datasets = pd.read_csv('../_data/wine/winequality-white.csv', index_col=None, header=0, sep=';')
 
@@ -25,20 +28,28 @@ from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=66)
 
-def outliers(data_out):
-    for i in data_out:
-        quantile_1, q2, quantile_3 = np.percentile(data_out, [25,50,75])
-        print("1사분위: ", quantile_1)
-        print("q2: ", q2)
-        print("3사분위: ", quantile_3)
-        iqr = quantile_3 - quantile_1
-        lower_bound = quantile_1 - (iqr *1.5)
-        upper_bound = quantile_3 + (iqr *1.5)
-        return np.where((data_out>upper_bound) | (data_out<lower_bound))
+def outlier(data_out):
+    lis = []
+    for i in range(data_out.shape[1]):
+        quartile_1, q2, quartile_3 = np.percentile(data_out[:, i], [25, 50, 75])
+        print("Q1 : ", quartile_1)
+        print("Q2 : ", q2)
+        print("Q3 : ", quartile_3)
+        iqr = quartile_3 - quartile_1
+        print("IQR : ", iqr)
+        lower_bound = quartile_1 - (iqr * 1.5)
+        upper_bound = quartile_3 + (iqr * 1.5)
+        print('lower_bound: ', lower_bound)
+        print('upper_bound: ', upper_bound)
 
-outliers_loc = outliers(x_train)
-# 아웃라이어 개수 count추가
-ic(outliers_loc)
+        m = np.where((data_out[:, i]>upper_bound) | (data_out[:, i]<lower_bound))
+        n = np.count_nonzero((data_out[:, i]>upper_bound) | (data_out[:, i]<lower_bound))
+        lis.append([i+1,'columns', m, 'outlier_num :', n])
+
+    return np.array(lis)
+
+outliers_loc = outlier(aaa)
+print("outlier at :", outliers_loc) 
 
 
 
