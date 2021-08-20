46809a1ed9bc2661935748ed14d94050b3ab8ecc
hjuju <tkackeowjs@naver.com>
2021-08-20 10:08:29 +0900

    softmax

--- keras2/keras68_4_softmax.py
+++ keras2/keras68_4_softmax.py
@@ -7,5 +7,7 @@ def softmax(x):
 x = np.arange(1,5)
 y = softmax(x)
 
+# 전체 데이터 중에 각각의 값을 총합을 1로하여 분류
+
 plt.pie(y, labels=y, shadow=True, startangle=90)
-plt.show() # 전체 데이터 중에 각각의 값을 총합을 1로하여 분류
+plt.show() 
