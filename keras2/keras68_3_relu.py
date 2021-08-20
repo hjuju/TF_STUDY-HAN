168813bcd28b0e9babe4f7e2eee0af91c8235818
hjuju <tkackeowjs@naver.com>
2021-08-20 10:08:12 +0900

    relu

---
+++ keras2/keras68_3_relu.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def relu(x):
+    return np.maximum(0,x)
+
+x = np.arange(-5, 5, 0.1)
+y = relu(x)
+
+plt.plot(x,y)
+plt.grid()
+plt.show()
+
+# 과제 리키렐루
+
+# elu, selu, reacky relu ...
+# 68_3_2~4로 만들기
