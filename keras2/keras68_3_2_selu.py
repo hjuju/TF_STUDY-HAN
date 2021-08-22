9b071615d16db43430d71d1733a8f186d6be38ca
hjuju <tkackeowjs@naver.com>
2021-08-22 21:11:53 +0900

    activation

--- keras2/keras68_3_2_selu.py
+++ keras2/keras68_3_2_selu.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def elu(x, alpha):
+def selu(x, alpha):
 
     return (x>0)*x + (x<=0)+(alpha+(np.exp(x)-1))
 
 x = np.arange(-5, 5, 0.1)
-y = elu(x, 1)
+y = selu(x, 2)
 
 plt.plot(x, y)
 plt.grid()
