b0eb315d08615fa8bdb4e8029738683222af230e
hjuju <tkackeowjs@naver.com>
2021-08-23 13:33:07 +0900

    autoencoder

--- AE/A03_ae_mlp.py
+++ AE/A03_ae_mlp.py
@@ -8,13 +8,11 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist
 
-
 # 1. 데이터
 (x_train, _), (x_test, _) = mnist.load_data()
 
 x_train = x_train.reshape(60000, 784).astype('float')/255
 x_test = x_test.reshape(10000,784).astype('float')/255
-
 
 # 2. 모델
 from tensorflow.keras.models import Model, Sequential
@@ -26,8 +24,17 @@ def autoencoder(hidden_layer_size):
     model.add(Dense(units=784, activation='sigmoid'))
     return model
 
+def autoencoder2(hidden_layer_size):
+    model = Sequential()
+    model.add(Dense(units=hidden_layer_size, input_shape=(784,),activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(units=784, activation='sigmoid'))
+    return model
 
-model = autoencoder(hidden_layer_size=104)      # pca 95%
+
+model = autoencoder2(hidden_layer_size=104)      # pca 95%
 
 model.compile(optimizer='adam', loss='mse')
 
